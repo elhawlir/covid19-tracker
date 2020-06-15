df155584288b5aec8da65fce0fa39d4f308e963f
Rashid Elhawli <33712172+elhawlir@users.noreply.github.com>
2020-06-15 22:33:07 +1000

    cleaned up dropdown lists
    now only one dropdown list that autopopulates. removed redundant code

--- covid19_tracking.py
+++ covid19_tracking.py
@@ -25,13 +25,7 @@ for name in country_search:
 
 country_names.insert(0,"Select a country") #instructional option
 
-variable = StringVar() #dropdown list variable
-
 # creates drop down list in application
-w = ttk.Combobox(app, textvariable = variable)
-w.pack()
-w.config(values = country_names)
-
 # autocompletes drop down list as you type, case insensitive
 combo = AutocompleteCombobox(app)
 combo.set_completion_list(country_names)
@@ -75,10 +69,10 @@ def data_collection(country):
 
 # response to pressing button on application
 def display():
-    dirty_data = data_collection(variable.get())
+    dirty_data = data_collection(combo.get())
     print(dirty_data)
     data = data_cleanup(dirty_data)
-    message = "Country: {}\nTotal infected = {}\nNew Case = {}\nTotal Deaths = {}\nNew Deaths = {}\nRecovered = {}\nActive Case = {}\nSerious Critical = {}".format(variable.get(), *data)
+    message = "Country: {}\nTotal infected = {}\nNew Case = {}\nTotal Deaths = {}\nNew Deaths = {}\nRecovered = {}\nActive Case = {}\nSerious Critical = {}".format(combo.get(), *data)
     print(message)
 
 # creates button that will display results
